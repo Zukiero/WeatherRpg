48571f6fa739be3141ae9598159ac8050fc25c71
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-04-13 20:06:34 +0200

    Setup game (#1)

--- main.rb
+++ main.rb
@@ -1,19 +1,82 @@
 require "tty-prompt"
 require_relative "player"
+require_relative "decision_maker"
 
 prompt = TTY::Prompt.new
 
 name = prompt.ask("What is your name")
-player_class = prompt.select("Choose your class", ["Warrior", "Priest", "Thief"])
+player_class = prompt.select("Choose your class", Player::CHARACTER_CLASS.keys)
 
 player = Player.new(name, player_class)
+opponent = Player.new("Opponent", Player::CHARACTER_CLASS.keys.sample)
 
-while player.life_points >= 0 do
-    action = prompt.select("action", ["Attack", "Defend", "Use potion"])
+count = 0
 
-    player.send(action.downcase.sub(" ", "_").to_sym)
-
-    puts player.info
+def puts_delay(string)
+  string.chars.each do |char|
+    print char
+    sleep 0.05
+  end
+  puts "\n"
 end
 
-puts "You died"
+def diff_info(previous_info, current_info)
+  result = []
+  current_info.each do |key, value|
+    result << "#{key}: #{value} (#{value - previous_info[key]})"
+  end
+
+  result.join(" | ")
+end
+
+def display_winning_banner(winner, count)
+  puts "######### WINNER #########"
+  puts "    #{winner.name} won in #{count} turns"
+  puts "#########################"
+end
+
+def winner(player, opponent)
+  if opponent.life_points <= 0
+    player
+  elsif player.life_points <= 0
+    opponent
+  else
+    false
+  end
+end
+
+def has_winner?(player, opponent)
+  winner(player, opponent) != false
+end
+
+until has_winner?(player, opponent) do
+  player_action = prompt.select("action", Player::CHARACTER_ACTION.keys)
+
+  opponent_action = DecisionMaker.new(Player::CHARACTER_ACTION.values, player, opponent).take_action
+
+  previous_player_info = player.info
+  previous_opponent_info = opponent.info
+
+  player.action(Player::CHARACTER_ACTION[player_action], opponent)
+  opponent.action(opponent_action, player)
+
+  system("clear")
+
+  puts_delay "   You choose #{player_action}"
+  puts_delay "    Opponent choose #{opponent_action}"
+
+  puts "######### Your info #########"
+  puts "    #{diff_info(previous_player_info, player.info)}"
+  puts "#############################"
+  puts "\n"
+  puts "######### Opponent info #########"
+  puts "    #{diff_info(previous_opponent_info, opponent.info)}"
+  puts "#############################"
+
+  puts_delay "End of turn #{count}"
+
+  puts_delay ("..........")
+  count += 1
+
+  display_winning_banner(winner(player, opponent), count) if has_winner?(player, opponent)
+end
--- player.rb
+++ player.rb
@@ -1,29 +1,72 @@
+require 'byebug'
+
 class Player
-    attr_accessor :name, :player_class, :life_points
+  attr_accessor :name, :player_class, :life_points
+  MAXIMUM_SHIELD_POINTS = 100
+  POTION_BONUS = 1.25
+  SHIELD_DAMAGE = 0.5
+  CHARACTER_CLASS = {
+    "Warrior" => {
+      life_points: 100,
+      shield_points: 100,
+      potion_percentage: 1.0,
+      attack_percentage: 1.0
+    }, "Priest" => {
+      life_points: 100,
+      shield_points: 0,
+      potion_percentage: 1.25,
+      attack_percentage: 0.75
+    }, "Thief" => {
+      life_points: 100,
+      shield_points: 0,
+      potion_percentage: 1.0,
+      attack_percentage: 1.5
+    }
+  }
+  CHARACTER_ACTION = { "Attack" => :attack, "Defend" => :defend, "Use potion" => :use_potion }
 
-    def initialize(name, player_class)
-        @name = name
-        @player_class = player_class
-        @life_points = 100
-        @shield_points = 0
-    end
+  def initialize(name, player_class)
+    @name = name
+    @player_class = player_class
+    @life_points = CHARACTER_CLASS[player_class][:life_points]
+    @shield_points = CHARACTER_CLASS[player_class][:shield_points]
+    @attack_percentage = CHARACTER_CLASS[player_class][:attack_percentage]
+    @potion_percentage = CHARACTER_CLASS[player_class][:potion_percentage]
+  end
 
-    def attack
-      puts "I have attacked!"
-    end
+  def attack(opponent)
+    opponent.hit(damage)
+  end
 
-    def defend
-        return unless @shield_points < 100
-        
-        @shield_points += 100
-    end
+  def hit(damage)
+    effective_damage = damage - (damage * @shield_points / 100)
+    @shield_points = @shield_points * SHIELD_DAMAGE
 
-    def use_potion
-        @life_points = @life_points + @life_points * 25 / 100
-    end
+    @life_points -= effective_damage
+  end
 
-    def info
-        {"Life Points" => @life_points, "Name"=> @name, "Shield Points"=> @shield_points, "Class"=> @player_class }
-    end
+  def defend
+    return if @shield_points > MAXIMUM_SHIELD_POINTS
+
+    @shield_points = 100
+  end
+
+  def use_potion
+    @life_points = @life_points * POTION_BONUS * @potion_percentage
+  end
+
+  def info
+    { "Life Points" => @life_points, "Shield Points" => @shield_points }
+  end
+
+  def damage
+    Random.rand(10..100) * @attack_percentage
+  end
+
+  def action(action, opponent)
+    send(action, opponent)
+  rescue ArgumentError
+    send(action)
+  end
 end
 
